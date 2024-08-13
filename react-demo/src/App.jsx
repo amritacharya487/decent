f33163217f615072b07e62d082e762d90d57fcbc
pawank98 <kharelpawan3760@gmail.com>
2024-08-13 10:48:26 +0545

    final comit

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -63,32 +63,4 @@ const App = () => {
 };
 
 export default App;
-/*const App =() => {
-  
-  return (
-    <div className="App">
-      <> 
-      <Navbar />
-        <Routes>
-          <Route  path='*' element={<Home/>}/>
-          <Route  path='/About' element={<About/>}/>
-          <Route  path='/Service' element={<Service/>}/>
-          <Route  path='/Contact' element={<Contact/>}/>  
-          <Route path='/Login' element={<Login/>} />
-          <Route path='/Register' element={<Register/>} />
-          <Route path='/Dashboard' exact element={<Dashboard/>} />
-          <Route path="/form" element={FormPage} />
-          <Route path='/Dashboard/profile/:username' element={<UserProfile/>} />
-          
-        </Routes>
-       {/* Your other components go here }
-       
-       
-      
-      </>
-      <Footer />
-    </div>
-  );
-}
 
-export default App;*/
